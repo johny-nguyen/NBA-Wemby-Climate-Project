589b1fc27025dd0b720cc7fcacfae1f565ae5cdb
Johny Nguyen <jon009@ucsd.edu>
2026-06-08 04:07:57 -0700

    fixed the scrollytelling

--- scrolly.js
+++ scrolly.js
@@ -22,11 +22,11 @@ const observer = new IntersectionObserver((entries) => {
             const currentStepId = entry.target.getAttribute('data-step');
             
             // Automatically switch the chart metric based on the section!
-            if (currentStepId === "1") {
+            if (currentStepId === "3") {
                 document.querySelector('input[value="x3pa_per_game"]').click();
-            } else if (currentStepId === "2") {
+            } else if (currentStepId === "1") {
                 document.querySelector('input[value="pace"]').click();
-            } else if (currentStepId === "3") {
+            } else if (currentStepId === "2") {
                 document.querySelector('input[value="fga_per_game"]').click();
             } else if (currentStepId === "4") {
                 document.querySelector('input[value="wins"]').click();
